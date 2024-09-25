fb5ca9c077387bbb79c2d1c3e52af8415fc9c752
Goutam <=>
2024-09-25 16:53:59 +0530

    Created Structure and added Home page

---
+++ src/Componets/Home/home.js
@@ -0,0 +1,21 @@
+import './home.css'
+
+const  Home = () => {
+    return (
+        <div className="nav-container" >
+        <div className="app-name">
+            Find Job
+        </div>
+        <div className="signup-login-container">
+            <div className="login">
+                Login
+            </div>
+            <div className="sign-up">
+                Sign Up
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Home
