d08f8017420cc47aecea70cb729e844fe0cc1b85
Goutam <goutamshet0@gmail.com>
2024-09-27 18:12:10 +0530

    Changes made in login.css

--- src/Componets/SignIn/login.js
+++ src/Componets/SignIn/login.js
@@ -4,10 +4,9 @@ const Login = () => {
     return (
         <div className="login-container" >
             <form className="card">
-                <h1 className="login-title">Login</h1>
+                <h1 className="login-title">Log In</h1>
                 <div className="input-container">
                     <input type="email" placeholder="Email" className="login-input"/>
-                    <br/>
                     <input type="password" placeholder="Password" className="login-input"/>
                 </div>
                 <button className="login-button">Log In</button>
