2502dd19eaaecd6a66284e5b3dd4e39471249f74
Goutam <goutamshet0@gmail.com>
2024-09-29 15:26:03 +0530

    Changed button

---
+++ src/Componets/Home/SearchButton/search_button.js
@@ -0,0 +1,12 @@
+import './search_button.css'
+
+const SearchButton = () => {
+    return (
+        <div className="button-container">
+            <button className="find-button">Search Job</button>
+        </div>
+        
+    )
+}
+
+export default SearchButton
